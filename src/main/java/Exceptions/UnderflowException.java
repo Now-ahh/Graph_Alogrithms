4ae2a0380e7d0dd5c45bfd1891db93a6777c4975
Now-ahh <noahbarrett256@gmail.com>
2026-09-24 13:56:19 +0100

    finished PriorityQueue class

---
+++ src/main/java/Exceptions/UnderflowException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class UnderflowException extends RuntimeException {
+  public UnderflowException(String message) {
+    super(message);
+  }
+}
--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,16 +1,4 @@
-import org.junit.Test;
-
-import Data_Structures.PriorityQueue;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class PriorityQueueTest {
-    PriorityQueue<Integer> intTest = new PriorityQueue<>();
-    PriorityQueue<String> strTest = new PriorityQueue<>();
-
-
-    @Test
-    public void getDataTest(PriorityQueue Q){
-        //assertEquals();
-    }
+  
 }
